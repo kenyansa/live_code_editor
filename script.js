61dd5fb66b2a5b721116c32f24989fc2362ad684
Kenyansa <kenyansajustine@gmail.com>
2023-07-14 05:45:41 +0300

    add function wrapping to reduce risks of using eval() directyly. it will minimize injection attacks.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+//add function wrapping to reduce risks of using eval() directyly. it will minimize injection attacks.
+function executeCode(code){
+    try{
+        return new function(code)();
+    }catch(error){
+        console.error(error);
+    }
+
+}
 function run(){
     let htmlCode = document.getElementById("html-code").value;
     let cssCode = document.getElementById("css-code").value;
     let jsCode = document.getElementById("js-code").value;
     let output = document.getElementById("output");
 
-    output.contentDocument.body.innerHTML = htmlCode+ "<style>" +cssCode + "</style>";
+    output.contentDocument.body.innerHTML = htmlCode
+
+    //create a new <style> element and set its content to the CSS code
+    let style = document.createElement("style");
+    style.innerHTML = cssCode;
+    
+    //append the <style> element to the <head> of the iframe document
+    output.contentDocument.appendChild(style);
+
     output.contentWindow.eval(jsCode);
     
 }
